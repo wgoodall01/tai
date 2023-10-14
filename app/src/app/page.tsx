aca9143dd2816e253b1ecc292011946b99140b88
Ibrahim Abotaleb <iawad@synapse-analytics.io>
2023-10-14 13:42:39 -0400

    Add Next.JS base for frontend

---
+++ app/src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     <div className='mr-auto'>
+        <h1 className='text-[52px] font-serif'>
+          Hi! This is Tai :3
+          </h1>
+        <p className='text-[24px] font-serif'>
+          I'm a personalized Teaching Assistant AI
+        </p>
+     </div>
+    </main>
+  )
+}

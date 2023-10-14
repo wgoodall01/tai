65407965b614bcd967309c67e54683fa285e7c4e
Ibrahim Abotaleb <iawad@synapse-analytics.io>
2023-10-14 14:53:30 -0400

    Add PP Fonts

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -2,15 +2,21 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <div className='mr-auto'>
-        <h1 className='text-[52px] font-serif'>
-          Hi! This is Tai :3
+    <main className="flex flex-col min-h-screen font-mono m-4">
+        <h1 className='text-[52px] font-bold italic'>
+          Hi I'm Tai!
           </h1>
-        <p className='text-[24px] font-serif'>
-          I'm a personalized Teaching Assistant AI
+        <p className='text-[24px] font-medium'>
+          Your own personalized Teaching Assistant AI
         </p>
+     
+     
+     {/* Getting Started Section */}
+     <div className='flex p-5 pl-0 items-center'>
+        <h2>To start: Copy the link of your Canvas course here --> </h2>
+        <input className='canvasInput ml-4 p-1' placeholder='Link goes here :3' />
      </div>
+     
     </main>
   )
 }

2d897742cdb58bdc1b9055ff17e3a3f45ad20eeb
William Goodall <wgoodall01@gmail.com>
2023-10-15 01:48:19 -0400

    gunicorn: disable start timeout

--- index/gunicorn.conf.py
+++ index/gunicorn.conf.py
@@ -5,3 +5,6 @@ accesslog = "-"
 
 # Listen on 0.0.0.0:$PORT or 0.0.0.0:8000
 bind = f"0.0.0.0:{os.environ.get('PORT', '8000')}"
+
+# Set a 5 min timeout---the data takes a while to load on initial startup.
+timeout = 60*5

17b5c6eb123669f61b16c2e8156fcfa5a61f4731
Ibrahim Abotaleb <iawad@synapse-analytics.io>
2023-10-14 23:07:57 -0400

    Add FSM to website cards

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,33 +1,20 @@
 'use client'
 
 import { useEffect, useState } from "react";
+import 'swiper/css';
+
+enum Status {
+  START,
+  COURSESELECT,
+  PROMPTING
+}
 
 export default function Home() {
 
   const [courseId, setCourseId] = useState(-1);
-  const [response, setResponse] = useState('')
-
-  useEffect(() => {
-    if (courseId !== -1) {
-      const socket =  new WebSocket("ws://127.0.0.1:5000/ask");
-  
-      socket.onopen = (event) => {
-        console.log("WebSocket connection opened:", event);
-        socket.send(courseId.toString());
-    };
-    
-    socket.onmessage = (event) => {
-        console.log("Received message:", event.data);
-        setResponse(event.data);
-    };
-    
-    socket.onclose = (event) => {
-        console.log("WebSocket connection closed:", event);
-    };
-  
-      }
-  }, [courseId])
-
+  const [status, setStatus] = useState(Status.START);
+  const [courseButtonEnabled, setCourseButtonEnabled] = useState(false);
+  const [question, setQuestion] = useState('');
 
   const detectCourseId = (courseLink : string) => {
     const courseNumber = courseLink.split('/');
@@ -35,38 +22,76 @@ export default function Home() {
     if (courseNumber.length < 5 || courseNumber[4] === '') {
       setCourseId(-1);
     } else {
-      setCourseId(parseInt(courseNumber[4]))
+      var id = parseInt(courseNumber[4]);
+      setCourseId(id);
+      if (Math.log10(id) > 5) {
+        setCourseButtonEnabled(true);
+      }
     }
   }
 
+  const boxContents = () => {
+    switch(status) {
+      case Status.START:
+        return(
+          <>
+            <h1 className='text-[52px] font-bold italic'>
+              Hi I'm Tai!
+              </h1>
+            <p className='text-[24px] font-medium'>
+                Your own personalized Teaching Assistant AI
+            </p>
+          
+          <div className="w-full flex justify-end">
+            <span className="button" onClick={() => setStatus(Status.COURSESELECT)}>Start</span>
+            </div>
+          </>
+        )
+      case Status.COURSESELECT:
+        return(
+          <>
+            <h1 className='text-[52px] font-bold italic'>
+              Course Selection
+              </h1>
+            <p className='text-[24px] font-medium'>
+                Please select the course you want to use Tai for
+            </p>
+            <input className='canvasInput w-full p-4 my-5' 
+            placeholder='Copy the link to your Canvas Course here'
+            type='url'
+            onChange={(e) => detectCourseId(e.target.value)}
+            />
+          <div className="w-full flex justify-between">
+            <span className="button" onClick={() => setStatus(Status.START)}>Back</span>
+            <span className={"button " + (!courseButtonEnabled && "disabled")} onClick={() => courseButtonEnabled && setStatus(Status.PROMPTING)}>Next</span>
+            </div>
+          </>
+        )
+        case Status.PROMPTING:
+          return(
+            <>
+            <h1 className='text-[52px] font-bold italic'>
+              Ask Away
+              </h1>
+            <p className='text-[24px] font-medium'>
+                Now you can ask Tai any questions you have about the course!
+            </p>
+            <input className='canvasInput w-full p-4 my-5' 
+            placeholder='Questions go here :D'
+            type='text'
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+            />
+          </>
+        )
+    }
+  }
 
   return (
-    <main className="flex flex-col min-h-screen font-mono m-4">
-        <h1 className='text-[52px] font-bold italic'>
-          Hi I'm Tai!
-          </h1>
-        <p className='text-[24px] font-medium'>
-          Your own personalized Teaching Assistant AI
-        </p>
-     
-     
-     {/* Getting Started Section */}
-     <div className='mt-5'>
-      <h2>Getting Started :p</h2>
-      <div className="mt-2">
-      <span>1.</span>
-        <input className='canvasInput p-1' 
-               placeholder='Copy the link to your Canvas Course here'
-               type='url'
-              onChange={(e) => detectCourseId(e.target.value)}
-               />
-      </div>
-      <div className="mt-2">
-        <span>2.</span>
-        <button>{response}</button>
-      </div>
-     </div>
-     
+    <main className="flex flex-col min-h-screen font-mono m-4 justify-center items-center">
+          <div className="glass min-w-[55vw]">
+              {boxContents()}
+          </div>
     </main>
   )
 }

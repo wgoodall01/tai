88d73715a19793d29f93a870c2171fb8ec9b6860
Ibrahim Abotaleb <iawad@synapse-analytics.io>
2023-10-15 01:19:06 -0400

    Add demo files

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "swiper/css";
 import axios from "axios";
 
@@ -31,8 +31,10 @@ export default function Home() {
   const [status, setStatus] = useState(Status.START);
   const [courseButtonEnabled, setCourseButtonEnabled] = useState(false);
   const [question, setQuestion] = useState('');
-  const [answer, setAnswer] = useState<PromptAnswer>();
   const [canAsk, setCanAsk] = useState(true);
+
+  const [promptAnswers, setPromptAnswers] = useState<PromptAnswer[]>([]);
+
 
   const getAnswer = async () => {
     const headers = {
@@ -54,9 +56,12 @@ export default function Home() {
         question: question,
         response: response.data
       }
-      setAnswer(newEntry);
-      console.log(answer);
+
+      // prepare to answer new question
+      setPromptAnswers([newEntry, ...promptAnswers]);
       setCanAsk(true);
+      setQuestion('');
+
   })
   
   }
@@ -69,7 +74,7 @@ export default function Home() {
     } else {
       var id = parseInt(courseNumber[4]);
       setCourseId(id);
-      if (Math.log10(id) > 5) {
+      if (Math.log10(id) > 4) {
         setCourseButtonEnabled(true);
       }
     }
@@ -137,30 +142,33 @@ export default function Home() {
               value={question}
               onChange={(e) => setQuestion(e.target.value)}
             />
-              {answer != undefined && 
-            <div className="glass !w-full mb-3">
-              <p className='text-[16px] font-bold mb-2'> 
-                {answer?.question}
-              </p>
-              <p className='text-[16px] font-medium mb-2'> 
-                {answer?.response.answer}
-              </p>
-              <p className="text-[14px] font-thin">
-                {
-                  answer?.response.citations.map((ref) => {
-                    return(
-                      <p className="text-[14px] font-thin">
-                        {'[' + ref.citation_no + '] '} {ref.file_name} {ref.page_number && ("| Page " + ref.page_number)}
-                      </p>
-                    )
-                  })
-                }
-              </p>
-            </div>
-            }
-            <div className="w-full flex justify-end">
+            <div className="w-full flex justify-end mb-5">
               <span className={"button " + (!canAsk && "disabled")} onClick={() => canAsk && getAnswer()}>Ask</span>
             </div>
+            {promptAnswers.map((answer) => {
+              return(
+                <div className="glass !w-full mb-3">
+                  <p className='text-[16px] font-bold mb-2'> 
+                    {answer?.question}
+                  </p>
+                  <p className='text-[16px] font-medium mb-2'> 
+                    {answer?.response.answer}
+                  </p>
+                  <p className="text-[14px] font-thin">
+                    {
+                      answer?.response.citations.map((ref) => {
+                        return(
+                          <p className="text-[14px] font-thin">
+                            {'[' + ref.citation_no + '] '} {ref.file_name} {ref.page_number && ("| Page " + ref.page_number)}
+                          </p>
+                        )
+                      })
+                    }
+                  </p>
+                </div>
+              )
+            })
+          }
           </>
         );
     }
@@ -169,6 +177,31 @@ export default function Home() {
   return (
     <main className="flex flex-col min-h-screen font-mono m-4 justify-center items-center">
       <div className="glass min-w-[55vw]">{boxContents()}</div>
+      {status === Status.COURSESELECT && (
+        <div className="glass min-w-[55vw] mt-5">
+          <p className="text-[17px] font-medium flex flex-col">
+            For demo purposes, you can use one of these courses:
+          </p>
+          <p className="font-thin my-3">
+            Computer Networks: https://gatech.instructure.com/courses/324194
+          </p>
+          <p className="font-thin my-3">
+            Intro Cyber Phys Sys Sec: https://gatech.instructure.com/courses/352034
+          </p>
+          <p className="font-thin my-3">
+            Mobile & Ubiquitous Comp: https://gatech.instructure.com/courses/334454
+          </p>
+          <p className="font-thin my-3">
+            Comp. Org & Program: https://gatech.instructure.com/courses/118080
+          </p>
+          <p className="font-thin my-3">
+            Data Structures and Algoirthms: https://gatech.instructure.com/courses/91004
+          </p>
+          <p className="font-thin my-3">
+            Objects & Design: https://gatech.instructure.com/courses/137180
+          </p>
+        </div>
+      )}
     </main>
   );
 }
